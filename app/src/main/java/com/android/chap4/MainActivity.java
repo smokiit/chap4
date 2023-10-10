355a50783ef33fe19c15d7f50af3ee4feeb64ecc
smokiit <smo.nicek08@yahoo.com>
2023-10-10 14:25:04 +0900

    chap 4

---
+++ app/src/main/java/com/android/chap4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.chap4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.p4_28);
+    }
+}
